cc60226b4717c8bc6d9141609be5adce2ab046b3
areckx <alex.loranger.1988@gmail.com>
2014-05-19 15:57:49 +0000

    init

---
+++ zerk.cpp
@@ -0,0 +1,30 @@
+// File: zerk.cpp
+// Description: [A text-based RPG]
+// Last Modified: 2014-05-16Fri 10:43 PDT
+//
+
+// This game should be able to ::
+// 	[] Lead the player to various locations
+//
+// 	[] Allow the player to inspect things
+//
+// 	[] Allow the player to hold and use items such as keys, weapons, food, etc.
+//
+// 	[] Display the player's current health status, including ::
+// 		
+// 		* Health points
+// 		* Hunger level
+//		* Energy level 
+//
+//	[] Use a time-tracking system to simulate the passing of time, day/night.
+//		
+//		* There are different challenges at night
+//
+//		* Some tasks can only be completed at either day/night
+//
+// 	[] Allow the player to save his/her progress
+//
+// 	[] Present challenging enemies such as ogres to battle
+//
+// 	[] Allow the player to interact with friendly characters, some of whome
+// 	   may aid the player on his/her journey
