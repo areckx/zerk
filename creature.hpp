c71ea1e7f613a51782fa526ea24b47d67fb562e1
areckx <alex.loranger.1988@gmail.com>
2014-05-20 11:25:39 +0000

    dialogue is an hpp file

---
+++ creature.hpp
@@ -0,0 +1,21 @@
+// File: creature.hpp
+// Description: [Creature class for Zerk]
+// Last Modified: 2014-05-16Fri 10:43 PDT
+//
+#ifndef CREATURE_HPP
+#define CREATURE_HPP
+
+#include <string>
+
+class Creature {
+
+	public: 
+
+		Creature() {
+
+		}
+
+
+
+};
+#endif  /* CREATURE_HPP */
